05bdda7d5fa23b935d0de5987d1c921373dfbee3
Alexander Adamik <aadamik@c2s9d36.42wolfsburg.de>
2024-02-02 18:03:32 +0100

    passed francinette

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadamik <aadamik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 23:57:27 by aadamik           #+#    #+#             */
+/*   Updated: 2024/02/02 17:55:15 by aadamik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 7
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_line1(char *str, int i);
+char	*get_str(char *str, int fd);
+char	*update_str(char *str);
+char	*get_next_line(int fd);
+int		ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s);
+void	*ft_calloc(int n, int size);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aadamik <aadamik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 19:39:51 by aadamik           #+#    #+#             */
+/*   Updated: 2024/02/02 17:56:17 by aadamik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*s1s2;
+
+	s1s2 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (s1s2 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		s1s2[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		s1s2[i] = s2[j];
+		i++;
+		j++;
+	}
+	s1s2[i] = '\0';
+	return (s1s2);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0' && s[i] != (char)c)
+	{
+		i++;
+	}
+	if (s[i] == (char)c)
+	{
+		return (&s[i]);
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	char	*str;
+
+	str = malloc(ft_strlen(s) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+void	*ft_calloc(int n, int size)
+{
+	void	*ptr;
+	int		i;
+
+	ptr = NULL;
+	ptr = malloc(n * size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < (n * size))
+	{
+		((char *)ptr)[i] = '\0';
+		i++;
+	}
+	return (ptr);
+}
